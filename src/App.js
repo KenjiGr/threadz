6867358ff398522c3402833aa0300eb61d3bb2a7
Kenji Greene <kenjigrr@gmail.com>
2020-04-25 19:05:35 -0400

    update our menu items to nav withRouter HOC'

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import './App.css';
+import {Route} from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component'
+
+
 
 function App() {
   return (
     <div >
-      <HomePage/>
+      <Route exact path='/' component={HomePage}/>
+      <Route exact path='/topics' component={HomePage}/>
+      <Route path='/topics/:topicid' component={HomePage}/>
     </div>
   );
 }
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -10,29 +10,34 @@ class Directory extends React.Component {
             sections: [{
                 title: 'All Jerseys',
                 imageUrl: 'https://images.unsplash.com/photo-1499754162586-08f451261482?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80',
-                id: 1
+                id: 1,
+                linkUrl: 'alljerseys'
             },
             {
                 title: 'Current Jerseys',
                 imageUrl: 'https://images.unsplash.com/photo-1551479460-5e76c686816a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1320&q=80',
-                id: 2
+                id: 2,
+                linkUrl: ''
             },
             {
                 title: 'Retro Jerseys',
                 imageUrl: 'https://images.unsplash.com/photo-1560973802-d3b7d065ef74?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80',
-                id: 3
+                id: 3,
+                linkUrl: ''
             },
             {
                 title: 'Other Jerseys',
                 imageUrl: 'https://images.unsplash.com/photo-1555356109-872a9d903d5a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1351&q=80',
                 size: 'large',
-                id: 4
+                id: 4,
+                linkUrl: ''
             },
             {
                 title: 'Mystery Box',
                 imageUrl: 'https://images.unsplash.com/photo-1583514735599-2ec2d33beaf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1349&q=80',
                 size: 'large',
-                id: 5
+                id: 5,
+                linkUrl: ''
             },
         ]}
     }
@@ -41,8 +46,8 @@ class Directory extends React.Component {
         return(
             <div className="directory-menu">
                 {
-                    this.state.sections.map(({title, imageUrl, id, size}) => (
-                        <MenuItem key={id} title={title} imageUrl={imageUrl} size={size}/>
+                    this.state.sections.map(({id, ...otherSectionProps}) => (
+                        <MenuItem key={id} {...otherSectionProps}/>
                     ))
                 }
             </div>
